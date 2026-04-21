1fbbc4c4badaadeb8477b9e50dcd4c1c2cc904cf
Angelica <angelica.giraldo10699@gmail.com>
2026-04-21 13:53:03 +0200

    lista

--- src/main/java/com/restaurantes/model/enums/DishType.java
+++ src/main/java/com/restaurantes/model/enums/DishType.java
@@ -1,5 +1,19 @@
 package com.restaurantes.model.enums;
 
+import java.awt.*;
+
 public enum DishType {
-    STARTER, MAIN, DESSERT
+    STARTER("Entrandes"),
+    MAIN("princicales"),
+    DESSERT("postres");
+
+    private final String label;
+
+    DishType(String label){
+        this.label = label;
+    }
+
+    public String getLabel(){
+        return label;
+    }
 }
